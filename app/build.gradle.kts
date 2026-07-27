3b1f98d3b96e6986d775427162d7750449856f95
4 0 4 \ 2.0 [🇲🇲] <146018190+nyeinkokoaung404@users.noreply.github.com>
2026-07-27 07:39:36 +0630

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     
-    // WireGuard Android SDK
-    implementation("com.wireguard.android:tunnel:1.0.20230707")
+    // WireGuard Official Android SDK (MavenCentral Version)
+    implementation("com.wireguard.android:tunnel:1.0.20230716")
 
     // Network / Coroutines
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
