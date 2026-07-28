391e4b6bc5972124efc5095202eb2e36722050c0
4 0 4 \ 2.0 [🇲🇲] <146018190+nyeinkokoaung404@users.noreply.github.com>
2026-07-29 01:32:46 +0630

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,10 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+
+        ndk {
+            abiFilters.addAll(setOf("arm64-v8a", "armeabi-v7a", "x86_64"))
+        }
     }
 
     buildTypes {
