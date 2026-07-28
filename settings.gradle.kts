23abb3f661c9ddda76ce7df628ad59a71d8f5ae7
4 0 4 \ 2.0 [🇲🇲] <146018190+nyeinkokoaung404@users.noreply.github.com>
2026-07-29 00:44:01 +0630

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,8 +11,9 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = java.net.URI("https://jitpack.io") }
     }
 }
 
-rootProject.name = "wgcf-vpn"
+rootProject.name = "warp-tunnel"
 include(":app")
